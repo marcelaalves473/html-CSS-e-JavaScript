f8d697d38f56cc160d158e3b1252cd16858970be
marcelaalves473 <139917253+marcelaalves473@users.noreply.github.com>
2023-08-03 16:26:48 +0300

    Add files via upload

---
+++ HTML, JavaSript, CSS/main.js
@@ -0,0 +1,11 @@
+document.getElementById("btn-laranja").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class","laranja");
+    })
+    
+    document.getElementById("btn-vermelho").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class","vermelho");
+    })
+    
+    document.getElementById("btn-purpura").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class","purpura");
+    })
